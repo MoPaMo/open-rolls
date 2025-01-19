f25d9aa028a12b19c56b090736ff4c9cb13b64da
MoPaMo <mopamo@users.noreply.github.com>
2025-01-19 11:14:05 +0100

    add initial character creation page structure

---
+++ app/character/new/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+export default function CharacterNew() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-6">
+   WIP
+    </div>
+  );
+}
