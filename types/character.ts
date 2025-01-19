71f6da3607880858892bd28f6bb06e57e1b28817
MoPaMo <mopamo@users.noreply.github.com>
2025-01-19 11:34:34 +0100

    add character interface to define character attributes and structure

---
+++ types/character.ts
@@ -0,0 +1,36 @@
+export interface Character {
+  name: string;
+  race: string;
+  class: string;
+  level: number;
+  background: string;
+  attributes: {
+    strength: number;
+    dexterity: number;
+    constitution: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
+  };
+
+  skills: string[];
+  proficiencies: {
+    languages: string[];
+    tools: string[];
+  };
+  spells: Array<{
+    name: string;
+    level: number;
+    components: string[];
+    description: string;
+  }>;
+  abilities: Array<{
+    name: string;
+    description: string;
+  }>;
+  alignment: {
+    ethic: -1 | 0 | 1;
+    moral: -1 | 0 | 1;
+  };
+  backstory: string;
+}
