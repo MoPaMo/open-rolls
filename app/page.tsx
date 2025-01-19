eafff80c72780916ef9a2ecb1166eb7ed811d1dd
MoPaMo <mopamo@users.noreply.github.com>
2025-01-19 11:05:48 +0100

    refactor home page layout and enhance styling for better user experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>OpenRolls</h1>
-      <p>Open Source DnD Character Creator</p>
-      <Link href={"/character/new"}>
-        <Button>Create your next character</Button>
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-6">
+      <h1 className="text-4xl font-semibold">OpenRolls</h1>
+      <p className="text-lg text-gray-600">Open Source DnD Character Creator</p>
+      <Link href="/character/new">
+        <Button className="mt-4 text-2xl">Create Your Next Character</Button>
       </Link>
     </div>
   );
