ab716c632e6182e80b7b57a62dfc10a08471bee1
MoPaMo <mopamo@users.noreply.github.com>
2025-01-19 11:04:40 +0100

    add button component and update home page with character creation link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
   return (
     <div>
       <h1>OpenRolls</h1>
+      <p>Open Source DnD Character Creator</p>
+      <Link href={"/character/new"}>
+        <Button>Create your next character</Button>
+      </Link>
     </div>
   );
 }
